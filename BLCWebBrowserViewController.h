23c1edc1274ba5aa33f7491d597d590421f4cd77
Casey Ward <caseyward1@gmail.com>
2015-03-10 20:47:55 -0700

    Functional web browser

--- BLCWebBrowserViewController.h
+++ BLCWebBrowserViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BLCWebBrowserViewController.h
 //  BlocBrowser
 //
 //  Created by Casey Ward on 3/9/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BLCWebBrowserViewController : UIViewController
 
 @end
-
